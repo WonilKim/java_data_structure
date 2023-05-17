9204ca39ee0cfdf993f83bb6e7005c2335936591
WonilKim <nonelove2@gmail.com>
2023-05-17 19:47:43 +0900

    230517 data structure

--- src/chapter04/Point.java
+++ src/chapter04/Point.java
@@ -12,9 +12,38 @@ public class Point {
         return y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRow() {
+        return y;
+    }
+
+    public int getCol() {
+        return x;
+    }
+
+    public void setRow(int y) {
+        this.y = y;
+    }
+
+    public void setCol(int x) {
+        this.x = x;
+    }
+
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
     }
 
     @Override
--- src/chapter05/BacktrackingQueen.java
+++ src/chapter05/BacktrackingQueen.java
@@ -9,179 +9,193 @@ import chapter04.Point;
 
 public class BacktrackingQueen {
 
-	private static ObjectStack stack = new ObjectStack(10);
+    private static ObjectStack stack = new ObjectStack(10);
 
-	public static void SolveQueen(int[][] data) {
-		int count = 0, mode = 0;
-		int icol = 0, irow = 0;
+    public static void SolveQueen(int[][] data) {
+        int count = 0, mode = 0;
+        int icol = 0, irow = 0;
 
-		Point p = new Point(icol, irow);
+        Point p = new Point();
+        p.setRow(irow);
+        p.setCol(icol);
 
-		data[irow][icol] = 1; // [0, 0] 에 초기 퀸 설정
-		stack.push(p);
-		System.out.println("push(" + p + ")");
-		count++;
+        data[irow][icol] = 1; // [0, 0] 에 초기 퀸 설정
+        stack.push(p);
+        System.out.println("push(" + p + ")");
+        count++;
 
-		while (count < data.length) {
+        // x 축은 좌/우 = row 방향
+        // y 축은 상/하 = column 방향
 
-			icol++; // x축 한 칸 이동 (우측)
-			int r = 0; // y측 0으로 초기화
+        while (count < data.length) {
 
-			while (icol < data.length) { // 체스판의 가로 길이보다 작을때 까지
+            icol++; // x축 한 칸 이동 (우측)
+            int r = 0; // y측 0으로 초기화
 
-				while (r < data[0].length) { // 체스판의 세로 길이보다 작은 동안
-                    
-					if (CheckMove(data, r, icol) == true) { // 이 위치에 퀸을 놓을 수 있는지 확인
-						p = new Point(icol, r);
-						System.out.println("push(" + p + ")");
-						stack.push(p); // 스택에 위치를 저장하고
-						data[r][icol] = 1;
-						count++; // 카운트 증가
-						break; // 루프 빠져나오기
-					}
-					r++;    // y축 한 칸 이동 (아래)
-				}
+            while (icol < data.length) { // 체스판의 가로 길이보다 작을때 까지
 
-				if (r != data[0].length) { // y축이 체스판의 세로 길이 내부에 있으면 (퀸을 놓을 자리를 찾았으면)
-					break; // 가로 길이 루프 종료
-				} else {    // 퀸을 놓을 자리를 찾지 못했으면
-					p = stack.pop(); // 스택에 저장된 위치(마지막으로 놓은 퀸)를 제거하고
-					System.out.println("pop(" + p + ")");
-					data[p.getY()][p.getX()] = 0;
-					count--; // 카운트 감소
+                while (r < data[0].length) { // 체스판의 세로 길이보다 작은 동안
 
-                    icol = p.getX();    // x 축은 제거된 위치와 같은 곳에서 다시 시작
-                    r = p.getY() + 1;   // y 축은 제거된 위치의 다음(아래) 부터 시작
-				}
-			} // while (ix < d.length)
-		} // while (count < data.length)
+                    if (CheckMove(data, r, icol) == true) { // 이 위치에 퀸을 놓을 수 있는지 확인
+                        p = new Point();
+                        p.setRow(r);
+                        p.setCol(icol);
+                        System.out.println("push(" + p + ")");
+                        stack.push(p); // 스택에 위치를 저장하고
+                        data[r][icol] = 1;
+                        count++; // 카운트 증가
+                        break; // 루프 빠져나오기
+                    }
+                    r++; // y축 한 칸 이동 (아래)
+                }
 
-		for(Point pp : stack.getAll()) {
-			data[pp.getY()][pp.getX()] = 1;
-		}
-	} // public static void SolveQueen(int[][] data)
+                if (r != data[0].length) { // y축이 체스판의 세로 길이 내부에 있으면 (퀸을 놓을 자리를 찾았으면)
+                    break; // 가로 길이 루프 종료
+                } else { // 퀸을 놓을 자리를 찾지 못했으면
+                    p = stack.pop(); // 스택에 저장된 위치(마지막으로 놓은 퀸)를 제거하고
+                    System.out.println("pop(" + p + ")");
+                    data[p.getRow()][p.getCol()] = 0;
+                    count--; // 카운트 감소
 
-	public static boolean checkRow(int[][] data, int row) {
+                    icol = p.getCol(); // x 축은 제거된 위치와 같은 곳에서 다시 시작
+                    r = p.getRow() + 1; // y 축은 제거된 위치의 다음(아래) 부터 시작
+                }
+            } // while (ix < d.length)
+        } // while (count < data.length)
 
-		for (int c = 0; c < data.length; c++) {
-			if (data[row][c] == 1) {
-				return false;
-			}
-		}
+    } // public static void SolveQueen(int[][] data)
 
-		return true;
-	}
+    public static boolean checkRow(int[][] data, int row) {
+        // 지정한 row 에 퀸이 놓여 있는지 확인
+        for (int c = 0; c < data.length; c++) {
+            if (data[row][c] == 1) {
+                return false;
+            }
+        }
 
-	public static boolean checkCol(int[][] data, int col) {
+        return true;
+    }
 
-		for (int r = 0; r < data[0].length; r++) {
-			if (data[r][col] == 1) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean checkCol(int[][] data, int col) {
+        // 지정한 column 에 퀸이 놓여 있는지 확인
+        for (int r = 0; r < data[0].length; r++) {
+            if (data[r][col] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public static boolean checkDiagSW(int[][] data, int row, int col) { // x++, y-- or x--, y++ where 0<= x,y <= 7
-		int c = col;
-		int r = row;
-		while ((c < data[0].length) && (0 <= r)) {
-			c++;
-			r--;
-			if ((c == data[0].length) || (r < 0)) {
-				break;
-			}
-			if (data[r][c] == 1) {
-				return false;
-			}
-		}
-		c = col;
-		r = row;
-		while ((0 <= c) && (r < data.length)) {
-			c--;
-			r++;
-			if ((c < 0) || (r == data.length)) {
-				break;
-			}
-			if (data[r][c] == 1) {
-				return false;
-			}
-		}
-		return true;
-	} // public static boolean checkDiagSW(int[][] data, int row, int col)
+    public static boolean checkDiagSW(int[][] data, int row, int col) { // x++, y-- or x--, y++ where 0<= x,y <= 7
+        int c = col;
+        int r = row;
+        // 지정한 위치의 북동쪽 방향에 퀸이 놓여 있는지 확인
+        while ((c < data[0].length) && (0 <= r)) {
+            c++;
+            r--;
+            if ((c == data[0].length) || (r < 0)) {
+                break;
+            }
+            if (data[r][c] == 1) {
+                return false;
+            }
+        }
+        c = col;
+        r = row;
+        // 지정한 위치의 서남쪽 방향에 퀸이 놓여 있는지 확인
+        while ((0 <= c) && (r < data.length)) {
+            c--;
+            r++;
+            if ((c < 0) || (r == data.length)) {
+                break;
+            }
+            if (data[r][c] == 1) {
+                return false;
+            }
+        }
+        return true;
+    } // public static boolean checkDiagSW(int[][] data, int row, int col)
 
-	public static boolean checkDiagSE(int[][] data, int row, int col) {// x++, y++ or x--, y--
-		int c = col;
-		int r = row;
-		while ((c < data[0].length) && (r < data.length)) {
-			c++;
-			r++;
-			if ((c == data[0].length) || (r == data.length)) {
-				break;
-			}
-			if (data[r][c] == 1) {
-				return false;
-			}
+    public static boolean checkDiagSE(int[][] data, int row, int col) {// x++, y++ or x--, y--
+        int c = col;
+        int r = row;
+        // 지정한 위치의 남동쪽 방향에 퀸이 놓여 있는지 확인
+        while ((c < data[0].length) && (r < data.length)) {
+            c++;
+            r++;
+            if ((c == data[0].length) || (r == data.length)) {
+                break;
+            }
+            if (data[r][c] == 1) {
+                return false;
+            }
 
-		}
-		c = col;
-		r = row;
-		while ((0 <= c) && (0 <= r)) {
-			c--;
-			r--;
-			if ((c < 0) || (r < 0)) {
-				break;
-			}
-			if (data[r][c] == 1) {
-				return false;
-			}
-		}
-		return true;
-	} // public static boolean checkDiagSE(int[][] data, int row, int col)
+        }
+        c = col;
+        r = row;
+        // 지정한 위치의 북서쪽 방향에 퀸이 놓여 있는지 확인
+        while ((0 <= c) && (0 <= r)) {
+            c--;
+            r--;
+            if ((c < 0) || (r < 0)) {
+                break;
+            }
+            if (data[r][c] == 1) {
+                return false;
+            }
+        }
+        return true;
+    } // public static boolean checkDiagSE(int[][] data, int row, int col)
 
-	public static boolean CheckMove(int[][] data, int row, int col) {// (x,y)로 이동 가능한지를 check
+    public static boolean CheckMove(int[][] data, int row, int col) {// (row, col) 위치로 이동 가능한지 확인
 
-		if ((data[row][col] == 0) && (checkRow(data, row)) && (checkCol(data, col))
-				&& (checkDiagSW(data, row, col)) && (checkDiagSE(data, row, col)))
-			return true;
-		else
-			return false;
-	}
+        if ((data[row][col] == 0) && (checkRow(data, row)) && (checkCol(data, col))
+                && (checkDiagSW(data, row, col)) && (checkDiagSE(data, row, col)))
+            return true;
+        else
+            return false;
+    }
 
-	// public static boolean NextMove(int[][] data, int row, int col) {// 다음 row에 대하여 이동할 col을 조사
-	// 	for (int c = 0; c < data[0].length; c++) {
-	// 		if (CheckMove(data, row, c) == true) {
-	// 			return true;
-	// 		}
-	// 	}
-	// 	return false;
-	// }
+    // public static boolean NextMove(int[][] data, int row, int col) {// 다음 row에
+    // 대하여 이동할 col을 조사
+    // for (int c = 0; c < data[0].length; c++) {
+    // if (CheckMove(data, row, c) == true) {
+    // return true;
+    // }
+    // }
+    // return false;
+    // }
 
-	public static int NextMove(int[][] data, int col) { // 지정한 col에 이동할 수 있는 row가 있는지 확인
+    public static int NextMove(int[][] data, int col) { // 지정한 column에 이동할 수 있는 row가 있는지 확인
 
-		for (int r = 0; r < data.length; r++) {
-			if (CheckMove(data, r, col) == true) {
-				return r;
-			}
-		}
-		return -1;
-	}
+        for (int r = 0; r < data.length; r++) {
+            if (CheckMove(data, r, col) == true) {
+                return r;
+            }
+        }
+        return -1;
+    }
 
-	public static void main(String[] args) {
-		int count = 8;
-		int row = count, col = count;
-		int[][] data = new int[row][col];
-		for (int r = 0; r < row; r++)
-			for (int c = 0; c < col; c++)
-				data[r][c] = 0;
+    public static void main(String[] args) {
+        // 체스판 정사각형 길이 (퀸의 수)지정
+        final int N = 8;
+        
+        int row = N, col = N;
 
-		SolveQueen(data);
+        // 체스판 0으로 리셋
+        int[][] data = new int[row][col];
+        for (int r = 0; r < row; r++)
+            for (int c = 0; c < col; c++)
+                data[r][c] = 0;
 
-		for (int r = 0; r < row; r++) {
-			for (int c = 0; c < col; c++) {
-				System.out.print(" " + data[r][c]);
-			}
-			System.out.println();
-		}
-	}
+        SolveQueen(data);
+
+        // Queen 문제 풀이 결과 디스플레이
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                System.out.print(" " + data[r][c]);
+            }
+            System.out.println();
+        }
+    }
 }
